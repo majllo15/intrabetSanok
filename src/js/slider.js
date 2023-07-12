c0f4f21c33a8437080b5ef5b92967f92096c00dc
Majllo <miloszszelka01@gmail.com>
2023-07-12 23:58:35 +0200

    changed path files

--- src/js/slider.js
+++ src/js/slider.js
@@ -46,24 +46,24 @@ const resetInterval = () => {
 
 const changeImages = () => {
 	if (window.innerWidth > 992) {
-		firstImg.setAttribute("src", "./dist/img/gruz-betonowy.jpg")
-		secondImg.setAttribute("src", "./dist/img/grys-zwirowy.jpg")
-		thirdImg.setAttribute("src", "./dist/img/mieszanka-lamana.jpg")
-		fourthImg.setAttribute("src", "./dist/img/produkcja.jpg")
+		firstImg.setAttribute("src", "../dist/img/gruz-betonowy.jpg")
+		secondImg.setAttribute("src", "../dist/img/grys-zwirowy.jpg")
+		thirdImg.setAttribute("src", "../dist/img/mieszanka-lamana.jpg")
+		fourthImg.setAttribute("src", "../dist/img/produkcja.jpg")
 		carouselWidth = 1905
 	}
 	if (window.innerWidth < 992) {
-		firstImg.setAttribute("src", "./dist/img/mini-gruz-betonowy.jpg")
-		secondImg.setAttribute("src", "./dist/img/mini-grys-zwirowy.jpg")
-		thirdImg.setAttribute("src", "./dist/img/mini-mieszanka-lamana.jpg")
-		fourthImg.setAttribute("src", "./dist/img/mini-produkcja.jpg")
+		firstImg.setAttribute("src", "../dist/img/mini-gruz-betonowy.jpg")
+		secondImg.setAttribute("src", "../dist/img/mini-grys-zwirowy.jpg")
+		thirdImg.setAttribute("src", "../dist/img/mini-mieszanka-lamana.jpg")
+		fourthImg.setAttribute("src", "../dist/img/mini-produkcja.jpg")
 		carouselWidth = 992
 	}
 	if (window.innerWidth < 576) {
-		firstImg.setAttribute("src", "./dist/img/576-gruz-betonowy.jpg")
-		secondImg.setAttribute("src", "./dist/img/576-grys-zwirowy.jpg")
-		thirdImg.setAttribute("src", "./dist/img/576-mieszanka-lamana.jpg")
-		fourthImg.setAttribute("src", "./dist/img/576-produkcja.jpg")
+		firstImg.setAttribute("src", "../dist/img/576-gruz-betonowy.jpg")
+		secondImg.setAttribute("src", "../dist/img/576-grys-zwirowy.jpg")
+		thirdImg.setAttribute("src", "../dist/img/576-mieszanka-lamana.jpg")
+		fourthImg.setAttribute("src", "../dist/img/576-produkcja.jpg")
 		carouselWidth = 576
 	}
 }
